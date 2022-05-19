bfde0dd8570e3388f77e939d224fecd85f7c7bd2
sebokdas1 <sebok.das.ff@gmail.com>
2022-05-19 08:02:38 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+
+app.use(cors());
+app.use(express.json());
+
+
+//For mongodb connection
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.ulnic.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const taskCollection = client.db("toDo").collection("task");
+
+
+        //Find multiple data
+        app.get('/task', async (req, res) => {
+            const query = {};
+            const cursor = taskCollection.find(query);
+            const tasks = await cursor.toArray();
+            res.send(tasks);
+        });
+
+
+        //Delete Single task
+        app.delete('/task/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await taskCollection.deleteOne(query);
+            res.send(result);
+        });
+
+        //Post new task
+        app.post('/task', async (req, res) => {
+            const newTask = req.body;
+            const result = await taskCollection.insertOne(newTask);
+            res.send(result);
+        });
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+app.get('/', (req, res) => {
+    res.send('server running');
+});
+
+run().catch(console.dir)
+app.listen(port, () => {
+    console.log('crd running', port);
+});
